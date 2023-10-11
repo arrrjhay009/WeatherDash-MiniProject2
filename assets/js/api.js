aa772e2add6d1a9cadca42b9493257885a4b3e9f
arrrjhay009 <stamaria.ryanjay09@gmail.com>
2023-10-11 23:43:46 +0800

    fix fetch error

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,4 @@
-
-'use strict';
+"use strict";
 
 const api_key = "ca76a8992a20704a4cbffc6bdd1a6f54";
 
@@ -10,26 +9,30 @@ const api_key = "ca76a8992a20704a4cbffc6bdd1a6f54";
     
 */
 
-export const fetchData = function(URL, callback) {
-    fetch(`${URL}&appid=${api_key}`)
-        .then(res => res.json())
-        .then(data => callback(data));
-}
+export const fetchData = function (url, callback) {
+  fetch(`${url}&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => {
+      console.error(error);
+      alert("Failed to retrieve data. Please try again later.");
+    });
+};
 
 export const url = {
-    currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
-    },
-    forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
-    },
-    airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
-    },
-    reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
-    },
-    geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
-    }
-}
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
